4396b01b3acc8aec80a0ed1d68cff01c7d94114e
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-21 09:43:01 +0200

    Nueva subida datos + Continuación desarrollo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -1,34 +1,27 @@
 //Desarrollo de las visualizaciones
 import * as d3 from 'd3';
-import { numberWithCommas2 } from '../helpers';
-//import { getInTooltip, getOutTooltip, positionTooltip } from './modules/tooltip';
+import { numberWithCommas3 } from '../helpers';
+import { getInTooltip, getOutTooltip, positionTooltip } from '../modules/tooltip';
 import { setChartHeight } from '../modules/height';
 import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
 import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
 //Colores fijos
-const COLOR_PRIMARY_1 = '#F8B05C', 
-COLOR_PRIMARY_2 = '#E37A42', 
-COLOR_ANAG_1 = '#D1834F', 
-COLOR_ANAG_2 = '#BF2727', 
-COLOR_COMP_1 = '#528FAD', 
-COLOR_COMP_2 = '#AADCE0', 
-COLOR_GREY_1 = '#B5ABA4', 
-COLOR_GREY_2 = '#64605A', 
-COLOR_OTHER_1 = '#B58753', 
-COLOR_OTHER_2 = '#731854';
+const COLOR_PRIMARY_1 = '#F8B05C',
+COLOR_ANAG_PRIM_3 = '#9E3515';
+let tooltip = d3.select('#tooltip');
 
 export function initChart(iframe) {
     //Lectura de datos
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_14/main/data/ratio_plazas_residencias_espana.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_14/main/data/ratio_plazas_residencias_espana_v2.csv', function(error,data) {
         if (error) throw error;
 
         data.sort(function(x, y){
             return d3.descending(+x.ratio, +y.ratio);
         });
 
-        let margin = {top: 20, right: 20, bottom: 20, left: 140},
+        let margin = {top: 10, right: 10, bottom: 20, left: 90},
             width = document.getElementById('chart').clientWidth - margin.left - margin.right,
             height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
 
@@ -43,26 +36,43 @@ export function initChart(iframe) {
             .domain([0, 12])
             .range([ 0, width]);
 
+        let xAxis = function(svg) {
+            svg.call(d3.axisBottom(x).ticks(4));
+            svg.call(function(g){
+                g.selectAll('.tick line')
+                    .attr('class', function(d,i) {
+                        if (d == 0) {
+                            return 'line-special';
+                        }
+                    })
+                    .attr('y1', '0')
+                    .attr('y2', `-${height}`)
+            });
+        }
+
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+            .call(xAxis);
 
         let y = d3.scaleBand()
                 .range([ 0, height ])
                 .domain(data.map(function(d) { return d.ccaa_prov; }))
                 .padding(.1);
 
+        let yAxis = function(svg) {
+            svg.call(d3.axisLeft(y));
+            svg.call(function(g){g.selectAll('.tick line').remove()});
+        }
+
         svg.append("g")
-            .call(d3.axisLeft(y))
-            .selectAll('.tick text')
-            .style('text-transform', function(d,i) { if(data[i].tipo != 'prov') { return 'uppercase'; } });
+            .call(yAxis);
 
         function init() {
             svg.selectAll("bars")
                 .data(data)
                 .enter()
                 .append("rect")
-                .attr('class','bars')
+                .attr('class','rect')
                 .attr("x", x(0) )
                 .attr("y", function(d) { return y(d.ccaa_prov) + y.bandwidth() / 4; })
                 .attr("width", function(d) { return x(0); })
@@ -71,17 +81,45 @@ export function initChart(iframe) {
                     if (d.tipo != 'nacional') {
                         return COLOR_PRIMARY_1;
                     } else {
-                        return COLOR_ANAG_2;
+                        return COLOR_ANAG_PRIM_3;
                     }
+                })
+                .on('mouseover', function(d,i,e) {
+                    //Opacidad de las barras
+                    let bars = svg.selectAll('.rect');  
+                    bars.each(function() {
+                        this.style.opacity = '0.4';
+                    });
+                    this.style.opacity = '1';
+
+                    //Texto
+                    // let html = '<p class="chart__tooltip--title">' + d.NOMAUTO_2 + '</p>' + 
+                    // '<p class="chart__tooltip--text">Un ' + numberWithCommas3(parseFloat(d.porc_total_grupo).toFixed(1)) + '% de habitantes de esta autonomía tiene 65 años o más.</p>' +
+                    // '<p class="chart__tooltip--text">En cuanto a la división por sexos, un ' + numberWithCommas3(parseFloat(d.porc_total_hombres).toFixed(1)) + '% de los hombres y un ' + numberWithCommas3(parseFloat(d.porc_total_mujeres).toFixed(1)) + '% de las mujeres tiene 65 o más.</p>';
+            
+                    // tooltip.html(html);
+
+                    //Tooltip
+                    positionTooltip(window.event, tooltip);
+                    getInTooltip(tooltip);
+                })
+                .on('mouseout', function(d,i,e) {
+                    //Quitamos los estilos de la línea
+                    let bars = svg.selectAll('.rect');
+                    bars.each(function() {
+                        this.style.opacity = '1';
+                    });
+                
+                    //Quitamos el tooltip
+                    getOutTooltip(tooltip);
                 })
                 .transition()
                 .duration(2000)
                 .attr("width", function(d) { return x(+d.ratio); });
-
         }
 
         function animateChart() {
-            svg.selectAll(".bars")
+            svg.selectAll(".rect")
                 .attr("width", function(d) { return x(0); })
                 .transition()
                 .duration(2000)
