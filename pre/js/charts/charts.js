1e9890fd74790c55df2345c9233ffaf17117d53e
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-05-25 17:06:47 +0200

    Cambio de repo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -14,7 +14,7 @@ let tooltip = d3.select('#tooltip');
 
 export function initChart() {
     //Lectura de datos
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_14/main/data/ratio_plazas_residencias_espana_v2.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/EnvejecimientoEnRed/informe_perfil_mayores_2022_social_4_14/main/data/ratio_plazas_residencias_espana_v2.csv', function(error,data) {
         if (error) throw error;
 
         data.sort(function(x, y){
